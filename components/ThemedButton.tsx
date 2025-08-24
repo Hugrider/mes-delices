fa379a5b41d5e1624eecf02c02642ca11d82100a
Hugo <hugo.dumartin@hotmail.fr>
2025-08-25 00:22:11 +0200

    feat: themed button component

---
+++ components/ThemedButton.tsx
@@ -0,0 +1,51 @@
+import { useThemeColors } from "@/constants/Theme";
+import React from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+type Props = {
+  text: string;
+  type?: "primary" | "default" | "delete";
+  onPress: () => void;
+};
+export default function ThemedButton({ text, type, onPress }: Props) {
+  const colors = useThemeColors();
+
+  function getBackgroundColor() {
+    if (type === "primary") return colors.primary;
+    if (type === "delete") return "red";
+    else return colors.onBackground;
+  }
+  function getTexteColor() {
+    if (type === "primary") return "#fff";
+    if (type === "delete") return "#fff";
+    else return colors.text;
+  }
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        { backgroundColor: getBackgroundColor(), opacity: pressed ? 0.5 : 1 },
+        styles.button,
+      ]}
+      onPress={onPress}
+    >
+      <Text style={[styles.text, { color: getTexteColor() }]}>{text}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  text: {
+    textAlign: "center",
+    textTransform: "uppercase",
+    letterSpacing: 0.5,
+    fontWeight: "500",
+  },
+});
